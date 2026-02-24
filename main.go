c90fb2778a99ec6bc995c6def7163f30b3f74143
bivol <vbivol394@gmail.com>
2026-02-24 14:19:55 +0200

    am schimba arhitectura logici cu ceva mai scalabil mai usor de folosit

--- main.go
+++ main.go
@@ -17,20 +17,16 @@ func main() {
 	structRequest.Method = args[1]
 	structRequest.URL = args[2]
 	structRequest.Headers = make(map[string]string)
-	for i := 3; i < len(os.Args); i++ {
-		if args[i] == "-H" {
-			x := strings.SplitN(args[i+1], ":", 2)
-			structRequest.Headers[x[0]] = strings.TrimSpace(x[1])
-			i++
-		}
-		if args[i] == "-D" || args[i] == "-d" {
-			structRequest.Body = append(structRequest.Body, args[i+1])
-			i++
+	for i := 3; i < len(args); i++ {
+		if def, ok := requests.Flags[args[i]]; ok {
+			def.Handler(&i, args, &structRequest)
 		}
 	}
+
 	if !strings.HasPrefix(structRequest.URL, "https://") && !strings.HasPrefix(structRequest.URL, "http://") {
 		structRequest.URL = "http://" + structRequest.URL
 	}
+
 	for _, method := range requests.GetMethodsActions() {
 		if structRequest.Method == method.Method {
 			method.Action(structRequest)
--- requests/options.go
+++ requests/options.go
@@ -1,8 +1,43 @@
 package requests
 
+import "strings"
+
 type RequestOptions struct {
 	Method  string
 	URL     string
 	Headers map[string]string
 	Body    []string
+}
+type FlagHandler func(i *int, args []string, req *RequestOptions)
+
+type FlagDef struct {
+	HasValue bool
+	Handler  FlagHandler
+}
+
+var Flags = map[string]FlagDef{
+	"-H": {true, handlerHeader},
+	"-d": {true, handlerBody},
+	"-I": {false, handlerIncludeHeaders},
+}
+
+func handlerHeader(i *int, args []string, req *RequestOptions) {
+	x := strings.SplitN(args[*i+1], ":", 2)
+	if len(x) != 2 {
+		return
+	}
+	if req.Headers == nil {
+		req.Headers = map[string]string{}
+	}
+	req.Headers[x[0]] = strings.TrimSpace(x[1])
+	*i++
+}
+
+func handlerBody(i *int, args []string, req *RequestOptions) {
+	req.Body = append(req.Body, args[*i+1])
+	*i++
+}
+
+func handlerIncludeHeaders(i *int, args []string, req *RequestOptions) {
+
 }
